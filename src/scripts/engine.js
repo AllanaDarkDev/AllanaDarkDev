1345e777e39a4aef051a9bc73afee827ab9d2b45
AllanaDarkDev <brallanadarksilvamoura@gmail.com>
2024-02-15 08:46:35 -0300

    feito função que nunca vai fucionar

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,30 @@
+const bgm =  document.getElementById("bgm")
+let divFlowers = document.getElementById("flowers")
 function playBgm() {
-    const bgm =  document.getElementById("bgm")
     bgm.play()    
 }
+
+function stopBgm() {
+    bgm.pause()
+}
+
+function addPetels() {
+    divFlowers.classList.add("blooming-flowers")
+}
+
+function removePetels() {
+    divFlowers.classList.remove("blooming-flowers")
+}
+
+togglePetels
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'a' || event.key === 'A') {
+      playBgm()
+      addPetels()
+    }
+    if (event.key === 'm' || event.key === 'M') {
+        stopBgm()
+        removePetels()
+    }
+});
