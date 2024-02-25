f67c2f219b20a917e86ec9d5db4de7965b3baa20
AllanaDarkDev <brallanadarksilvamoura@gmail.com>
2024-02-25 11:35:13 -0300

    atualização portifolio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,13 @@
 const bgm =  document.getElementById("bgm")
-let divFlowers = document.getElementById("flowers")
+let button = document.getElementById("dark/ligth-theme")
+const header = document.getElementById("header")
+const main = document.getElementById("main")
+const footer = document.getElementById("footer")
+const sobreMim = document.getElementsByClassName("sobre-mim")[0]
+const elementosP = document.querySelectorAll("p");
+const elementosH2 = document.querySelectorAll("h2");
+const elementosH3 = document.querySelectorAll("h3");
+
 function playBgm() {
     bgm.play()    
 }
@@ -8,22 +16,27 @@ function stopBgm() {
     bgm.pause()
 }
 
-function addPetels() {
-    divFlowers.classList.add("blooming-flowers")
-}
-
-function removePetels() {
-    divFlowers.classList.remove("blooming-flowers")
-}
-
-togglePetels
+button.addEventListener('mousedown', function() {
+    header.classList.toggle("header-dark")
+    main.classList.toggle("main-dark")
+    footer.classList.toggle("footer-dark")
+    sobreMim.classList.toggle("sobre-mim-dark")
+    
+    elementosP.forEach(function(elemento) {
+        elemento.classList.toggle("dark-font");
+    })
+    elementosH2.forEach(function(elemento) {
+        elemento.classList.toggle("dark-font");
+    });
+    elementosH3.forEach(function(elemento) {
+        elemento.classList.toggle("dark-font");
+    });
+})
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'a') {
-      playBgm()
-      addPetels()
+    if (event.key === 'p') {
+        playBgm()
     }else if (event.key === 'm') {
         stopBgm()
-        removePetels()
     }
 });
